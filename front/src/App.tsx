6f50c8250c6e4bf9f1d912a78817db3c830f2a95
pablarce <113457050+pablarce@users.noreply.github.com>
2025-02-24 15:41:33 +0100

    feat: app beggining + sonner (toast)

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,8 +1,42 @@
-function App() {
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
+
+import ProtectedLayout from "./components/layout/ProtectedLayout"
+import PublicLayout from "./components/layout/PublicLayout"
+import { Toaster } from "./components/ui/Sonner/Sonner"
+import { AuthProvider } from "./context/AuthContext"
+import { LibraryProvider } from "./context/LibraryContext"
+import { LoansProvider } from "./context/LoansContext"
+import Paths from "./router/paths"
+import routes from "./router/routes"
+
+const App = () => {
     return (
-        <div>
-            <p className="p-4 bg-red-500">hola</p>
-        </div>
+        <AuthProvider>
+            <LibraryProvider>
+                <LoansProvider>
+                    <BrowserRouter>
+                        <Routes>
+                            <Route path="*" element={<Navigate to={Paths.HOME} replace />} />
+
+                            {/* Rutas públicas */}
+                            <Route element={<PublicLayout />}>
+                                {routes.public.map((route, i) => (
+                                    <Route key={`public-${i}`} {...route} />
+                                ))}
+                            </Route>
+
+                            {/* Rutas protegidas */}
+                            <Route element={<ProtectedLayout />}>
+                                {routes.private.map((route, i) => (
+                                    <Route key={`private-${i}`} {...route} />
+                                ))}
+                            </Route>
+                        </Routes>
+                    </BrowserRouter>
+                    <Toaster />
+                </LoansProvider>
+            </LibraryProvider>
+        </AuthProvider>
     )
 }
 
